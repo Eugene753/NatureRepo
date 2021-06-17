cb527690f71580b73a1dd23adbf44b0c018bd0d1
Eugene753 <Imarkadanov@gmail.com>
2021-06-16 23:59:28 -0400

    Nature Project tree class created

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Tree tree=new Tree();
+        tree.type="oak";
+        tree.color="brown";
+        tree.size="mid size";
+    }
+}
---
+++ src/com/company/Tree.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Tree {
+
+    String type;
+    String size;
+    String color;
+
+}
